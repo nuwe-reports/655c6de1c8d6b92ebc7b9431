fdf9a352703838469983cd2341aa80533a0fec1b
Georman <georman.calderon@cleteci.com>
2023-11-25 04:18:00 -0400

    fixing sql setup

--- setup.sql
+++ setup.sql
@@ -2,6 +2,6 @@ CREATE DATABASE IF NOT EXISTS accwe-hospital;
 
 -- create the users for each database
 CREATE USER 'accweuser'@'%' IDENTIFIED BY 'somepassword';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `projectone`.* TO 'accweuser'@'%';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `accwe-hospital`.* TO 'accweuser'@'%';
 
 FLUSH PRIVILEGES;
